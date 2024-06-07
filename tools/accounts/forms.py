9561b47e78143ba42bacf14a7a14f96bf7947a51
harukiozawawork <ajeeharu@gmail.com>
2024-06-07 10:41:44 +0900

    ユーザー登録完成

--- tools/accounts/forms.py
+++ tools/accounts/forms.py
@@ -5,18 +5,8 @@ from django import forms
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["login_user","email","name","public_hall","password"]
+        fields = ["login_user","email","name","public_hall","password1","password2"]
 
-   #CustomUserに存在しないpassword2は追記
-        password2 = forms.CharField(
-            label='確認用パスワード',
-            required=True,
-            strip=False,
-        )
-        widgets = {
-           'password': forms.PasswordInput(),
-           'password2': forms.PasswordInput()
-       }
 
 
     def __init__(self, *args, **kwargs):
@@ -24,22 +14,17 @@ class SignUpForm(UserCreationForm):
         self.fields['public_hall'].queryset = PublicHall.objects.all()
 
         self.fields['login_user'].widget.attrs['class'] = self.fields['email'].widget.attrs['class']\
-         = self.fields['public_hall'].widget.attrs['class'] = self.fields['password'].widget.attrs['class']\
-         = self.fields['password2'].widget.attrs['class']\
+         = self.fields['public_hall'].widget.attrs['class'] = self.fields['name'].widget.attrs['class']\
+         = self.fields['password1'].widget.attrs['class'] = self.fields['password2'].widget.attrs['class']\
          = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 
         self.fields['login_user'].widget.attrs['placeholder'] = 'USER ID'
         self.fields['email'].widget.attrs['placeholder'] = 'xxx@yyy.zzz'
+        self.fields['name'].widget.attrs['placeholder'] = '清水　すげ'
         self.fields['public_hall'].widget.attrs['required'] = True
-        self.fields['password'].widget.attrs['placeholder'] = 'パスワード'
+        self.fields['password1'].widget.attrs['placeholder'] = 'パスワード'
         self.fields['password2'].widget.attrs['placeholder'] = '確認用パスワード'
 
-    def clean(self):
-        password = self.cleaned_data['password']
-        password2 = self.cleaned_data['password2']
-        if password != password2:
-            raise forms.ValidationError("パスワードと確認用パスワードが合致しません")
-        super().clean()
 
 # ログインフォームを追加
 class LoginFrom(AuthenticationForm):
--- tools/accounts/views.py
+++ tools/accounts/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView,CreateView
 from django.contrib.auth.views import LoginView as BaseLoginView,  LogoutView as BaseLogoutView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, LoginFrom
-
+from django.http import HttpResponseRedirect
 
 class IndexView(TemplateView):
     # """ ホームビュー """
@@ -16,16 +16,13 @@ class SignupView(CreateView):
     """ ユーザー登録用ビュー """
     form_class = SignUpForm # 作成した登録用フォームを設定
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("accounts:index") # ユーザー作成後のリダイレクト先ページ
+    success_url = reverse_lazy("accounting:index") # ユーザー作成後のリダイレクト先ページ
 
     def form_valid(self, form):
-        # ユーザー作成後にそのままログイン状態にする設定
-        response = super().form_valid(form)
-        login_user = form.cleaned_data.get("login_user")
-        password = form.cleaned_data.get("password1")
-        user = authenticate(login_user=login_user, password=password)
+        user = form.save() # formの情報を保存
         login(self.request, user)
-        return response
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
 
 class LoginView(BaseLoginView):
     form_class = LoginFrom
