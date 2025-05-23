6916ea7ce3a543e5b040712ecccd1d29de171690
shimizu.minami <smina-k@mx4.fctv.ne.jp>
2025-05-23 13:37:55 +0900

    DBをSQLiteに変更

--- tools/accounts/urls.py
+++ tools/accounts/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     # path("", views.IndexView.as_view(), name="index"),
     path('signup/', views.SignupView.as_view(), name="signup"),
     path('login/', views.LoginView.as_view(), name="login"),
-		path('logout/', views.LogoutView.as_view(), name="logout"),
-		path('hall/', views.HallListView.as_view(), name="hall"),
-		path('hall/modal/create', views.ModalHallCreateView.as_view(), name="modal_create_hall"),
-		path('hall/modal/<int:pk>/update', views.ModalHallUpdateView.as_view(), name="modal_update_hall"),
-		path('hall/modal/<int:pk>/delete', views.ModalHallDeleteView.as_view(), name="modal_delete_hall"),
+	path('logout/', views.LogoutView.as_view(), name="logout"),
+	path('hall/', views.HallListView.as_view(), name="hall"),
+	path('hall/modal/create', views.ModalHallCreateView.as_view(), name="modal_create_hall"),
+	path('hall/modal/<int:pk>/update', views.ModalHallUpdateView.as_view(), name="modal_update_hall"),
+	path('hall/modal/<int:pk>/delete', views.ModalHallDeleteView.as_view(), name="modal_delete_hall"),
 ]
 
 router = DefaultRouter()
