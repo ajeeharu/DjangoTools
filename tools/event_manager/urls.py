443af6b3d51537052ef339f665e456fda1f81962
shimizu.minami <smina-k@mx4.fctv.ne.jp>
2025-06-01 16:56:12 +0900

    calendar header toolbar CSS調整

--- tools/event_manager/urls.py
+++ tools/event_manager/urls.py
@@ -5,5 +5,6 @@ app_name = "event_manager"
 
 urlpatterns = [
     path('', views.CalendarView.as_view(), name="calnendar"),
+    path('2', views.Calendar2View.as_view(), name="calnendar2"),
 ]
 
--- tools/event_manager/views.py
+++ tools/event_manager/views.py
@@ -20,3 +20,12 @@ class CalendarView(LoginRequiredMixin,TemplateView):
         # page_title を追加する
         context['page_title'] = '各種設定'
         return context
+
+class Calendar2View(LoginRequiredMixin,TemplateView):
+    template_name = "event_manager/calendar(tailwindcss).html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        # page_title を追加する
+        context['page_title'] = '各種設定'
+        return context
