321903623a5a3ffe09472a24314995976b9eee24
shimizu.minami <smina-k@mx4.fctv.ne.jp>
2025-06-05 13:31:34 +0900

    ヘッダー追加でUIを会計処理との整合性

--- tools/event_manager/urls.py
+++ tools/event_manager/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "event_manager"
 
 urlpatterns = [
-    path('', views.CalendarView.as_view(), name="calnendar"),
-    path('2', views.Calendar2View.as_view(), name="calnendar2"),
+    path('', views.CalendarView.as_view(), name="calendar"),
+    path('2', views.Calendar2View.as_view(), name="calendar2"),
 ]
 
--- tools/event_manager/views.py
+++ tools/event_manager/views.py
@@ -18,7 +18,7 @@ class CalendarView(LoginRequiredMixin,TemplateView):
     def get_context_data(self):
         context = super().get_context_data()
         # page_title を追加する
-        context['page_title'] = '各種設定'
+        context['page_title'] = '公民館ツール：日程管理'
         return context
 
 class Calendar2View(LoginRequiredMixin,TemplateView):
@@ -27,5 +27,5 @@ class Calendar2View(LoginRequiredMixin,TemplateView):
     def get_context_data(self):
         context = super().get_context_data()
         # page_title を追加する
-        context['page_title'] = '各種設定'
+        context['page_title'] = 'カレンダー'
         return context
