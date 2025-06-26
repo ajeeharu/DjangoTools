30d35abf9849e4ab3c669af0792e0f3772709e31
shimizu.minami <smina-k@mx4.fctv.ne.jp>
2025-06-26 14:39:22 +0900

    休館日設定完了

--- tools/event_manager/views.py
+++ tools/event_manager/views.py
@@ -36,10 +36,11 @@ class HolidayCalendarListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         today = datetime.date.today()
-        current_public_hall = self.request.user.public_hall # ログイン中の公民館を取得
+        current_public_hall = self.request.user.public_hall  # ログイン中の公民館を取得
         if current_public_hall:
             queryset = HolidayCalendar.objects.filter(public_hall=current_public_hall).filter(
-            date__year=today.year, public_hall=current_public_hall).order_by('date').all() # QuerySet（一致するレコード全て取得）
+                # QuerySet（一致するレコード全て取得）
+                date__year=today.year, public_hall=current_public_hall).order_by('date').all()
         return queryset
 
     def get_context_data(self):
@@ -54,60 +55,87 @@ class HolidayCalendarListView(LoginRequiredMixin, ListView):
         return context
 
     def post(self, request):
-        today = datetime.date.today()
-        holidays = jpholiday.year_holidays(today.year)
+        current_year = datetime.date.today().year
         current_public_hall = self.request.user.public_hall
+# 該当(年度、所属公民館）するデータを削除
+        # print('delete')
+        HolidayCalendar.objects.filter(
+            date__year=current_year, public_hall=current_public_hall).delete()
+# 祝日データを取得
+        # print('holiday update_or_create')
+        holidays = jpholiday.year_holidays(current_year)
         for holiday in holidays:
             HolidayCalendar.objects.update_or_create(
                 date=holiday[0], public_hall=current_public_hall,
-                defaults={"name": holiday[1],
-                          "public_hall": current_public_hall,
-                          },
+                defaults={"name": holiday[1]},
             )
-
+# 定休日を設定
+        # print('regular holiday update_or_create')
         regularHoliday_list = RegularHoliday.objects.all()
         for regularHoliday in regularHoliday_list:
             for month in range(1, 13):
-                weekday_count = 0
-                _, end_day = calendar.monthrange(today.year, month)
+                weekday_count = 1
+                _, end_day = calendar.monthrange(current_year, month)
                 for day in range(1, end_day+1):
-                    date = datetime.datetime(today.year, month, day)
+                    date = datetime.datetime(current_year, month, day)
                     if date.weekday() == int(regularHoliday.day_of_week):
-                        weekday_count = weekday_count+1
                         if int(regularHoliday.semiweekly) == weekday_count or int(regularHoliday.day_of_week) == 0:
-                            holiday, created = HolidayCalendar.objects.filter(date=date,public_hall=current_public_hall).get_or_create(date=date,name='休館日',public_hall=public_hall)
-            
+                            try:
+                                holiday = HolidayCalendar.objects.filter(
+                                    date=date, public_hall=current_public_hall).get()
+                                # 祝日の振替か確認する。
+                                if (holiday.name.find('振替') == (-1)):
+                                    # print('振替')
+                                    date = datetime.datetime(
+                                        current_year, month, day+1)
+                                    # 翌日が祝日でなければ振替休館日にする
+                                    try:
+                                        holiday = HolidayCalendar.objects.filter(
+                                          date=date, public_hall=current_public_hall).get()
+                                    except HolidayCalendar.DoesNotExist:
+                                        HolidayCalendar.objects.create(
+                                           date=date, name='振替休館日', public_hall=current_public_hall)
+                            except HolidayCalendar.DoesNotExist:
+                                # print('休館日')
+                                HolidayCalendar.objects.create(
+                                    date=date, name='休館日', public_hall=current_public_hall)
+                        weekday_count = weekday_count+1
+
         return HttpResponseRedirect(self.success_url)
 
-class ModalHolidayCalendarCreateView(LoginRequiredMixin,CreateView):
+
+class ModalHolidayCalendarCreateView(LoginRequiredMixin, CreateView):
     model = HolidayCalendar
     form_class = HolidayCalendarForm
     success_url = reverse_lazy('event_manager:holidaycalendar')
 
     def form_valid(self, form):
-        form.save() # formの情報を保存
+        form.save()  # formの情報を保存
         return HttpResponseRedirect(self.success_url)
 
     def form_invalid(self, form):
         messages.add_message(self.request, messages.ERROR, form.errors)
         return HttpResponseRedirect(self.success_url)
 
-class ModalHolidayCalendarUpdateView(LoginRequiredMixin,UpdateView):
+
+class ModalHolidayCalendarUpdateView(LoginRequiredMixin, UpdateView):
     model = HolidayCalendar
     form_class = HolidayCalendarUpdateForm
     success_url = reverse_lazy('event_manager:holidaycalendar')
 
     def form_valid(self, form):
-        form.save() # formの情報を保存
+        form.save()  # formの情報を保存
         return HttpResponseRedirect(self.success_url)
 
     def form_invalid(self, form):
         messages.add_message(self.request, messages.ERROR, form.errors)
         return HttpResponseRedirect(self.success_url)
 
-class ModalHolidayCalendarDeleteView(LoginRequiredMixin,DeleteView):
+
+class ModalHolidayCalendarDeleteView(LoginRequiredMixin, DeleteView):
     model = HolidayCalendar
     success_url = reverse_lazy('event_manager:holidaycalendar')
+
 
 class HolidayCalendarApiView(viewsets.ModelViewSet):
     queryset = HolidayCalendar.objects.all()
